554d2aaee7409a1d8bc83f455f8d3ffc79c4bb81
DSGC <a222220993@unison.mx>
2026-03-06 08:13:31 -0700

    fix: cambios en endpoints, creacion de la API

--- script.js
+++ script.js
@@ -3,37 +3,41 @@ const API_URL = "http://localhost:3000/tasks";
 const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
-const message = document.getElementById("message");
+const message = document.getElementById("emptyMessage");
 
-document.addEventListener("DOMContentLoaded", () => {
-    loadTasks();
-});
+window.addEventListener("DOMContentLoaded", loadTasks);
 
-function loadTasks() {
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-            taskList.innerHTML = "";
+// GET
+async function loadTasks() {
 
-            if (data.length === 0) {
-                message.textContent = "No hay tareas registradas";
-            } else {
-                message.textContent = "";
-                data.forEach(task => {
-                    addTaskToDOM(task);
-                });
-            }
-        })
-        .catch(error => {
-            console.log("Error al cargar tareas:", error);
-        });
+    const response = await fetch(API_URL);
+    const tasks = await response.json();
+
+    taskList.innerHTML = "";
+
+    if (tasks.length === 0) {
+        emptyMessage.style.display = "block";
+        message.textContent = "No hay tareas registradas";
+        return;
+    }
+    emptyMessage.style.display = "none";
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+        li.innerHTML = `${task.title} 
+        <button class="delete-btn" onclick="deleteTask(${task.id})">
+            Eliminar</button>
+        `;
+        taskList.appendChild(li);
+    });
 }
 
-addBtn.addEventListener("click", () => {
-    const title = taskInput.value.trim();
+// POST
+async function addTask() {
 
-    if (title === "") {
-        alert("Escribe una tarea");
+    const title = taskInput.value;
+
+    if (title.trim() === "") {
+        alert("Por favor, ingresa una tarea válida.");
         return;
     }
 
@@ -42,49 +46,16 @@ addBtn.addEventListener("click", () => {
         completed: false
     };
 
-    fetch(API_URL, {
+    await fetch(API_URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(newTask)
-    })
-    .then(response => response.json())
-    .then(data => {
-        addTaskToDOM(data);
-        taskInput.value = "";
-        message.textContent = "";
-    })
-    .catch(error => {
-        console.log("Error al agregar tarea:", error);
     });
-});
+    taskInput.value = "";
+    loadTasks();
 
-function addTaskToDOM(task) {
-    const li = document.createElement("li");
-
-    li.textContent = task.title;
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Eliminar";
-    deleteBtn.classList.add("delete-btn");
-
-    deleteBtn.addEventListener("click", () => {
-        deleteTask(task.id);
-    });
-
-    li.appendChild(deleteBtn);
-    taskList.appendChild(li);
 }
 
-function deleteTask(id) {
-    fetch(`${API_URL}/${id}`, {
-        method: "DELETE"
-    })
-    .then(() => {
-        loadTasks();
-    })
-    .catch(error => {
-        console.log("Error al eliminar tarea:", error);
-    });
-}
+addBtn.addEventListener("click", addTask);
